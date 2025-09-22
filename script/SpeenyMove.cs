600f53729a444f42f53efd222ca75f26aeddef05
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-09-22 16:04:49 +0200

    Ray casting speeny

--- script/SpeenyMove.cs
+++ script/SpeenyMove.cs
@@ -1,27 +1,50 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpeenyMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float sens = 1f; // vers la droite
+    [SerializeField] float speed = 1f;
+    [SerializeField] bool goRight = true;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
-        // transform.position += Time.deltaTime * speed * transform.right;
+        transform.Translate(speed * (goRight ? 1f : -1f) * Time.deltaTime, 0f, 0f);
 
-        Vector3 move = speed * sens * Time.deltaTime * transform.right;
-        transform.Translate(move);
+        Vector3 origin = transform.position + 0.4f * Vector3.up + Vector3.right * 0.4f * (goRight ? 1f : -1f);
+        Vector3 direction = Vector3.right * (goRight ? 1f : -1f);
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, 0.02f);
+
+        if (hit.collider != null)
+        {
+            InverseSpeed();
+        }
+
+        Debug.DrawRay(origin, direction * 0.05f, Color.cyan);
+
+        origin = transform.position + Vector3.right * 0.4f * (goRight ? 1f : -1f);
+        direction = Vector3.down;
+
+        hit = Physics2D.Raycast(origin, direction, 1.01f);
+
+        if (hit.collider == null)
+        {
+            InverseSpeed();
+        }
+        Debug.DrawRay(origin, direction * 1.01f, Color.red);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void InverseSpeed()
     {
-        if (collision.gameObject.CompareTag("TurnBlock"))
-        {
-            sens *= -1;
-
-            Vector3 flip = transform.localScale;
-            flip.x *= -1;
-            transform.localScale = flip; 
-        }
+        goRight = !goRight;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
     }
 }
