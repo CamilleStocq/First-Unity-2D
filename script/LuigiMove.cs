220aae142341407ed01deb686a7ec3acac4ae0a3
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-09-22 13:59:37 +0200

    jump + go down luigi

--- script/LuigiMove.cs
+++ script/LuigiMove.cs
@@ -1,35 +1,86 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+
 public class LuigiMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
-    public InputActionAsset actions;
+    [SerializeField] private InputActionAsset actions;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce = 200f;
+
     private InputAction xAxis;
+    private SpriteRenderer renderer;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool isJumping = false;
+    private bool goingDown = false; // exo
 
     void Awake()
     {
-        xAxis = actions.FindActionMap("LuigiWalk").FindAction("XAxis");
+        xAxis = actions.FindActionMap("Luigi").FindAction("xAxis");
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        // boxCollider = GetComponent<BoxCollider2D>(); // exo
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
-        actions.FindActionMap("LuigiWalk").Enable();
+        actions.FindActionMap("Luigi").Enable();
+        actions.FindActionMap("Luigi").FindAction("Jump").performed += OnJump;
+        actions.FindActionMap("Luigi").FindAction("Down").performed += GoDown; //exo
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
-        actions.FindActionMap("LuigiWalk").Disable();
+        actions.FindActionMap("Luigi").Disable();
+        actions.FindActionMap("Luigi").FindAction("Jump").performed -= OnJump;
+        actions.FindActionMap("Luigi").FindAction("Down").performed -= GoDown;//exo
     }
 
     void Update()
     {
         MoveX();
+
+        if (isJumping)
+        {
+            if (rb.linearVelocityY < 0)
+            {
+                isJumping = false;
+                animator.SetBool("OnJump", false);
+            }
+        }
+    }
+    
+     private void OnJump(InputAction.CallbackContext context)
+    {
+        if (!isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            animator.SetBool("OnJump", true);
+            isJumping = true;
+        }
+    }
+
+    private void GoDown(InputAction.CallbackContext context) //exo
+    {
+        if (!goingDown)
+        {
+            animator.SetBool("GoDown", true);
+            goingDown = true;
+            // boxCollider.size = new Vector3(boxCollider.size.x, ySize);
+        }
     }
 
     private void MoveX()
     {
-        float xMove = xAxis.ReadValue<float>();
-        transform.position += speed * Time.deltaTime * xMove * transform.right;
+        renderer.flipX = xAxis.ReadValue<float>() < 0;
+        animator.SetFloat("Speed", Mathf.Abs(xAxis.ReadValue<float>()));
+        transform.Translate(xAxis.ReadValue<float>() * speed * Time.deltaTime, 0f, 0f);
     }
 }
